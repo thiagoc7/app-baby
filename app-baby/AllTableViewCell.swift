affafbac91faa49e80c1b4e5fd1a2487624f7f41
Thiago <thiagobocorrea@gmail.com>
2015-07-06 21:28:43 -0300

    basic navigation ok

---
+++ app-baby/AllTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AllTableViewCell.swift
+//  app-baby
+//
+//  Created by Thiago Boucas Correa on 7/6/15.
+//  Copyright (c) 2015 Thiago Boucas Correa. All rights reserved.
+//
+
+import UIKit
+
+class AllTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ app-baby/AllTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  AllTableViewController.swift
+//  app-baby
+//
+//  Created by Thiago Boucas Correa on 7/5/15.
+//  Copyright (c) 2015 Thiago Boucas Correa. All rights reserved.
+//
+
+import UIKit
+
+class AllTableViewController: UITableViewController {
+
+    var completedTimers = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        completedTimers = ["timer 1", "timer 2", "timer 3"]
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return completedTimers.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell =
+        self.tableView.dequeueReusableCellWithIdentifier(
+            "timerCell", forIndexPath: indexPath)
+            as! AllTableViewCell
+        
+        let row = indexPath.row
+        
+        cell.timerLabel.text = completedTimers[row]
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
